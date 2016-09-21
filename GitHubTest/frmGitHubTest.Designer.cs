5e56a83a6620ea2e7d4388dbb8e9cf06e5387dd0
ramachandran.krishnan <ramachandran.krishnan@gmail.com>
2016-09-21 16:44:54 -0400

    First Commit...

---
+++ GitHubTest/frmGitHubTest.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GitHubTest
+{
+    partial class frmGitHubTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(98, 115);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmGitHubTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnClose);
+            this.Name = "frmGitHubTest";
+            this.Text = "Git Hub Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
--- GitHubTest/frmGitHubTest.cs
+++ GitHubTest/frmGitHubTest.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace GitHubTest
 {
-    public partial class Form1 : Form
+    public partial class frmGitHubTest : Form
     {
-        public Form1()
+        public frmGitHubTest()
         {
             InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
